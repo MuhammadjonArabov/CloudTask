4db529a4579d28d4817b6a464a5aef12fabda6e5
SamandarDeveloper19 <mamadiyorovsamandar19@gmail.com>
2024-12-11 11:26:56 +0500

    FOUNDATION: Add Output Matrix Code

--- Program.cs
+++ Program.cs
@@ -4,7 +4,59 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            OutputSpiralMatrix();
+        }
+
+        static void OutputSpiralMatrix()
+        {
+            Console.Write("Matritsa o'lchamini kiriting: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            int num = 1;
+            int rowStart = 0, rowEnd = n - 1, colStart = 0, colEnd = n - 1;
+
+            while (rowStart <= rowEnd && colStart <= colEnd)
+            {
+                for (int i = colStart; i <= colEnd; i++)
+                {
+                    matrix[rowStart, i] = num++;
+                }
+                rowStart++;
+
+                for (int i = rowStart; i <= rowEnd; i++)
+                {
+                    matrix[i, colEnd] = num++;
+                }
+                colEnd--;
+
+                if (rowStart <= rowEnd)
+                {
+                    for (int i = colEnd; i >= colStart; i--)
+                    {
+                        matrix[rowEnd, i] = num++;
+                    }
+                    rowEnd--;
+                }
+
+                if (colStart <= colEnd)
+                {
+                    for (int i = rowEnd; i >= rowStart; i--)
+                    {
+                        matrix[i, colStart] = num++;
+                    }
+                    colStart++;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
